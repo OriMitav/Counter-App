ab256cfd7858a44c3bfefaa34b5a1626b28ef598
Ori Mitav <grei12@gmail.com>
2023-07-29 23:51:30 +0300

    Completing the counting method

--- app/src/main/java/com/example/counterapp/MainActivity.java
+++ app/src/main/java/com/example/counterapp/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.counterapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Declarations
+    TextView welcome_text, counter_text;
+    Button btn;
+    int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+        counter_text = findViewById(R.id.counter_text);
+        welcome_text = findViewById(R.id.welcome_text);
+
+        //Adding the functionalities
+        btn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                counter_text.setText(""+increaseCounter());
+            }
+        });
+
+    }
+    private int increaseCounter() {
+        return ++counter;
     }
 }
